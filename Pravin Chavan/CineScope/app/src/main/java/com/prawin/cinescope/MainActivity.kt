90db694fa918a2fe75c1f33eaf548b5aa481dcfd
pravin <cpravin8592@gmail.com>
2025-03-26 09:15:50 +0530

    Movie Details Screen UI completed.

--- Pravin Chavan/CineScope/app/src/main/java/com/prawin/cinescope/MainActivity.kt
+++ Pravin Chavan/CineScope/app/src/main/java/com/prawin/cinescope/MainActivity.kt
@@ -5,23 +5,28 @@ import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.prawin.cinescope.screens.MovieDetails
+import com.prawin.cinescope.screens.MovieHeader
 import com.prawin.cinescope.screens.splashScreen
 import com.prawin.cinescope.ui.theme.CineScopeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             CineScopeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    splashScreen(innerPadding)
-                }
+                /*Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MovieDetails(innerPadding)
+                }*/
+                MovieDetails(PaddingValues(0.dp))
             }
         }
     }
